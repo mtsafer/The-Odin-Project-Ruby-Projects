5691ced84b73a667343f1ad2f537a10c11f9b8a3
Martin Safer <mtsafer@gmail.com>
2016-06-13 16:25:16 -0700

    Complete event manager from jumpstartlabs

--- serialization-projects/event_manager/lib/event_manager.rb
+++ serialization-projects/event_manager/lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -8,28 +9,45 @@ def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5,"0")[0,5]
 end
 
+#cleans up the phone numbers or returns nil for a bad entry
+def clean_phone_numbers(phone)
+	return phone if phone.length == 10
+	return phone[1,phone.length] if phone.length == 11 && phone[0] == "1"
+	nil
+end
+
+#returns an array of the legislators from the given zipcode
 def legislators_by_zipcode(zipcode)
-	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+	Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
 
-	legislator_names = legislators.collect do |legislator|
-		"#{legislator.first_name} #{legislator.last_name}"
+#saves custom thank you letters to "output"
+def save_thank_you_letters(id, form_letter)
+	Dir.mkdir "output" unless Dir.exists? "output"
+
+	filename = "output/thanks_#{id}.html"
+
+	File.open(filename, 'w') do |file|
+		file.puts form_letter
 	end
-
-	legislator_names.join(", ")
 end
 
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv",
 					 headers: true, header_converters: :symbol
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+	id = row[:id]
 	name = row[:first_name]
 
 	zipcode = clean_zipcode(row[:zipcode])
 
-	legislator = legislators_by_zipcode(zipcode)
+	legislators = legislators_by_zipcode(zipcode)
 
-	puts "name: #{name}, \n    zip: #{zipcode} \
-	\n    legislators: #{legislator}\n\n"
+	form_letter = erb_template.result(binding)
+	
+	save_thank_you_letters(id, form_letter)
 end
