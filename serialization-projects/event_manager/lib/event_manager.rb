118861af5d8694b813bade915a471091c5424053
Martin Safer <mtsafer@gmail.com>
2016-06-13 15:36:45 -0700

    Implement Sunlight gem to find Legislators

--- serialization-projects/event_manager/lib/event_manager.rb
+++ serialization-projects/event_manager/lib/event_manager.rb
@@ -1,8 +1,21 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 #cleans up the zipcode
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5,"0")[0,5]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+
+	legislator_names.join(", ")
 end
 
 puts "EventManager Initialized!"
@@ -15,5 +28,8 @@ contents.each do |row|
 
 	zipcode = clean_zipcode(row[:zipcode])
 
-	puts "name: #{name}, \n   zip: #{zipcode}\n\n"
+	legislator = legislators_by_zipcode(zipcode)
+
+	puts "name: #{name}, \n    zip: #{zipcode} \
+	\n    legislators: #{legislator}\n\n"
 end
