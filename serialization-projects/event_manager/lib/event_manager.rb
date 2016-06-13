fc16b8079f45c018319f5a707f2f72d56ed05be2
Martin Safer <mtsafer@gmail.com>
2016-06-13 15:00:41 -0700

    Clean up zipcodes

--- serialization-projects/event_manager/lib/event_manager.rb
+++ serialization-projects/event_manager/lib/event_manager.rb
@@ -1,1 +1,19 @@
+require 'csv'
+
+#cleans up the zipcode
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5,"0")[0,5]
+end
+
 puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv",
+					 headers: true, header_converters: :symbol
+
+contents.each do |row|
+	name = row[:first_name]
+
+	zipcode = clean_zipcode(row[:zipcode])
+
+	puts "name: #{name}, \n   zip: #{zipcode}\n\n"
+end
